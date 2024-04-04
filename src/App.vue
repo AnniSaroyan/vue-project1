2031782811e33b846290223eba88cfe5b8ac4b9e
AnniSaroyan <annisaroyan.v@gmail.com>
2024-04-05 00:16:13 +0400

    liest

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,20 @@
 <script >
 
+import Exchange from './components/Exchange.vue'
+
 export default {
+
+  components: {
+    Exchange
+  },
   data() {
     return {
-      email: '',
-      isValid: false,
-      emailColor: '',
-      validColor: 'green',
-      invalidColor: 'red',
+
 
     }
   },
 
-  methods: {
 
-    checkEmail() {
-      let inputEmail = prompt('Enter your email:');
-      if (inputEmail !== null) {
-        this.email = inputEmail.trim();
-        this.isValid = this.validateEmail(this.email)
-        this.emailColor = this.isValid ? 'green' : 'red';
-      }
-    },
-
-    validateEmail(email) {
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailPattern.test(email);
-    },
-    reset() {
-      this.email = '';
-      this.isValid = false;
-      this.emailColor = '';
-    },
-
-  },
 }
 
 
@@ -41,11 +22,9 @@ export default {
 
 <template>
   <header>
-    <div> <button class="email" @click="checkEmail">Input Email</button> </div>
-    <h1 :style="{ border: isValid ? '2px solid green' : '2px solid red' }">{{ email }}</h1>
-    <h4 v-if="email" :style="{ color: validColor }">{{ isValid ? 'The email is valid' : '' }} </h4>
-    <h4 v-if="email" :style="{ color: invalidColor }">{{ isValid ? '' : 'The email is invalid' }}</h4>
-    <button @click="reset">Reset</button>
+
+    <Exchange />
+
 
 
   </header>
@@ -58,11 +37,4 @@ export default {
   </main>
 </template>
 
-<style>
-.email {
-  margin-bottom: 5px;
 
-  width: 150px;
-
-}
-</style>
